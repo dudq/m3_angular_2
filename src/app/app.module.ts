81d48fdaa2fce900cdb053e15ec6e395338554a8
du <dudq.xmt@gmail.com>
2019-12-25 20:08:18 +0700

    add blog file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {ImgSliderModule} from './img-slider/img-slider.module';
+import {BlogComponent} from './blog/blog.component';
+import {BlogEditComponent} from './blog-edit/blog-edit.component';
+import {BlogDetailComponent} from './blog-detail/blog-detail.component';
+import {HttpClientModule} from '@angular/common/http';
+import {AppRoutingModule} from './app-routing/app-routing.module';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
+    BlogComponent,
+    BlogEditComponent,
+    BlogDetailComponent,
   ],
   imports: [
     BrowserModule,
-    ImgSliderModule
+    ImgSliderModule,
+    HttpClientModule,
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
